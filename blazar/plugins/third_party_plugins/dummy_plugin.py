cc52958e3532d1946e18e5587caff324287b26f5
Mark Powers <markpowers@uchicago.edu>
2021-12-02 17:21:21 +0000

    Clean up DummyPlugin

--- blazar/plugins/third_party_plugins/dummy_plugin.py
+++ blazar/plugins/third_party_plugins/dummy_plugin.py
@@ -13,31 +13,8 @@ class DummyPlugin(base.BasePlugin):
     def resource_type(self):
         return "dummy"
 
-    def validate_data(self, data, action_type):
-        LOG.info("validate")
-        LOG.info(type(data))
-        LOG.info(type(action_type))
-        if action_type == "create":
-            ex_fn = exceptions.InvalidCreateResourceData
-        elif action_type == "update":
-            ex_fn = exceptions.InvalidUpdateResourceData
-
-        valid_params = set(["name", "score"])
-        data_params = set(data.keys())
-        # Check required params
-        for item in valid_params:
-            if item not in data_params:
-                raise ex_fn(f"Dummy plugin requires '{item}'")
-        # Check no extra params
-        extra_params = data_params - valid_params
-        if extra_params:
-            raise ex_fn(f"Invalid keys in data '{extra_params}'")
-
     def validate_create_params(self, data):
-        self.validate_data(data, "create")
+        return self.validate_data(data, ["name", "score"], [], "create")
 
     def validate_update_params(self, data):
-        self.validate_data(data, "update")
-
-    def deallocate(self, resources, lease):
-        LOG.info("allocating dummy")
+        return self.validate_data(data, ["name", "score"], [], "update")
