7523048ea9d9f023796fc5e098f88fc28a311ce1
Mark Powers <markpowers@uchicago.edu>
2025-01-13 15:33:32 -0600

    Fix is_project_allowed param (#121)

--- blazar/plugins/oshosts/host_plugin.py
+++ blazar/plugins/oshosts/host_plugin.py
@@ -717,7 +717,7 @@ class PhysicalHostPlugin(base.BasePlugin, nova.NovaClientWrapper):
         else:
             hosts = db_api.reservable_host_get_all_by_queries(filter_array)
         for host in hosts:
-            if not self.is_project_allowed(project_id, resource_properties):
+            if not self.is_project_allowed(project_id, self.get_computehost(host["id"])):
                 continue
             if not db_api.host_allocation_get_all_by_values(
                     compute_host_id=host['id']):
