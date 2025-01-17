e6ae4cfebf545525131ee6b61ffb40efccfbabdf
Mark Powers <markpowers@uchicago.edu>
2025-01-17 17:18:16 +0000

    Fix flavor reservations for Chameleon Cloud
    - Fix migration branches
    - Add migration to allow affinity to be nullable
    - Fix on_* events in flavor plugin
    - Replace usage of context with lease (not sure context seems broken)
    - Add before_end event to flavor reservations
    - Fix db session code for Antelope
    - Fix config assumptions

--- blazar/db/migration/alembic_migrations/versions/553383923ca0_add_compute_host_inventory_and_traits.py
+++ blazar/db/migration/alembic_migrations/versions/553383923ca0_add_compute_host_inventory_and_traits.py
@@ -16,14 +16,14 @@
 """Add compute host inventory and traits
 
 Revision ID: 553383923ca0
-Revises: 02e2f2186d98
+Revises: ee3b2513b59f
 Create Date: 2024-04-29 17:40:05.148493
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '553383923ca0'
-down_revision = '02e2f2186d98'
+down_revision = 'ee3b2513b59f'
 
 from alembic import op
 import sqlalchemy as sa
