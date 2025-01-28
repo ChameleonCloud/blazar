f226745d26a041cd9bd107cd426df968835c39db
Mark Powers <markpowers@uchicago.edu>
2025-01-28 22:45:52 +0000

    Add update_reservation to flavor plugin

--- blazar/manager/service.py
+++ blazar/manager/service.py
@@ -489,6 +489,7 @@ class ManagerService(service_utils.RPCServer):
             exceptions.InvalidDate,
             exceptions.CantUpdateParameter,
             exceptions.InvalidPeriod,
+            exceptions.NotImplemented,
             enforcement.exceptions.MaxLeaseDurationException,
             enforcement.exceptions.MaxLeaseUpdateWindowException,
             enforcement.exceptions.ExternalServiceUnsupportedHTTPResponse,
