0330a17447849301b65b53719b7445e3735645a5
Mark Powers <markpowers@uchicago.edu>
2025-05-27 19:59:08 +0000

    Add required flavor resource
    Change-Id: I190dca0ba944d8131ad3f0aedbe5e43eac147192

--- blazar/plugins/flavor/flavor_plugin.py
+++ blazar/plugins/flavor/flavor_plugin.py
@@ -39,6 +39,10 @@ plugin_opts = [
     cfg.BoolOpt('randomize_host_selection',
             default=False,
             help='Allocate hosts for reservations randomly.'),
+    cfg.StrOpt('placement_reservation_permitted_trait',
+            default=None,
+            help='If set, only permit flavor reservation for flavors with this trait.'),
+
 ]
 
 CONF = cfg.CONF
@@ -142,8 +146,7 @@ class FlavorPlugin(base.BasePlugin):
         # resource requests, e.g. baremetal vs virtual
         # or missing traits
         if resource_traits:
-            raise mgr_exceptions.NotImplemented(
-                error="Resource traits not supported yet")
+            LOG.warning("Resource traits not supported yet. Ignoring.")
         hosts = db_api.reservable_host_get_all_by_queries([])
 
         # find reservations for each host in our time period
@@ -297,6 +300,14 @@ class FlavorPlugin(base.BasePlugin):
         # options that are available in a flavor.
         resource_request, resource_traits = \
             self._estimate_flavor_resources(source_flavor)
+
+        if (
+            CONF[self.resource_type].placement_reservation_permitted_trait and
+            resource_traits.get(
+                CONF[self.resource_type].placement_reservation_permitted_trait
+            ) != "required"
+        ):
+                raise mgr_exceptions.InvalidFlavor(flavor=source_flavor["name"])
 
         return (resource_request, resource_traits, source_flavor)
 
