503a8ee96e6efba1692e12d066f8b6eeb9f5d020
Mark Powers <markpowers@uchicago.edu>
2025-07-18 12:39:01 -0500

    Add support for traits in flavor plugin
    Previously, traits could not be included with a flavor reservation. This
    commit implements checking flavor traits when querying available hosts.
    
    This check is done via the placement API, in parallel where possible. As
    a result, `_query_available_hosts` is slower when `resource_traits` is
    non-empty.
    
    Co-Authored-By: Michael Sherman <shermanm@uchicago.edu>
    Change-Id: I5905d97d85955113d07b418cbe65fbafe132cd4b
    Signed-off-by: Mark Powers <markpowers@uchicago.edu>

--- blazar/utils/openstack/placement.py
+++ blazar/utils/openstack/placement.py
@@ -150,18 +150,32 @@ class BlazarPlacementClient(object):
         LOG.error(msg, args)
         raise exceptions.ResourceProviderRetrievalFailed(name=rp_name)
 
-    def get_reservation_provider(self, host_name):
-        """Calls the placement API for a reservation provider record.
+    def list_resource_providers(
+            self, query="", microversion=PLACEMENT_MICROVERSION):
+        """Get all resource providers.
 
-        :param host_name: Name of the host
-        :return: A dict of resource provider information
-                 or None if the resource provider doesn't exist.
-        :raise: ResourceProviderRetrievalFailed on error.
+        :param query: A string of query parameters.
+        :param microversion: The microversion to use for the request.
+        :return: A list of resource provider information
+        :raise: ResourceProviderListFailed on error.
         """
+        resp = self.get(
+            f'/resource_providers?{query}', microversion=microversion)
+        if resp:
+            json_resp = resp.json()
+            if json_resp['resource_providers']:
+                return json_resp['resource_providers']
+            else:
+                return []
 
-        return self.get_resource_provider(
-            self._get_reservation_provider_name(host_name)
-        )
+        msg = ("Failed to get resource providers. "
+               "Got %(status_code)d: %(err_text)s.")
+        args = {
+            'status_code': resp.status_code,
+            'err_text': resp.text,
+        }
+        LOG.error(msg, args)
+        raise exceptions.ResourceProviderListFailed()
 
     def create_resource_provider(self, rp_name, rp_uuid=None,
                                  parent_uuid=None):
