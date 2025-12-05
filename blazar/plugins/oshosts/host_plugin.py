8b744d12b1786434d63539e226d9455c42ea7db1
Mark Powers <markpowers@uchicago.edu>
2025-12-05 21:01:04 +0000

    Add host filter to flavor/host reservations
    Change-Id: Icbebb86386d99a006a84b161ee05ec7c005acbae

--- blazar/plugins/oshosts/host_plugin.py
+++ blazar/plugins/oshosts/host_plugin.py
@@ -81,6 +81,9 @@ plugin_opts = [
         help='Allow users to create host reservations. This plugin must be enabled '
              'for flavor reservations, but it may not be desirable to allow an '
              'entire host to be reserved.'),
+    cfg.BoolOpt('filter_vm_hosts',
+            default=False,
+            help='Only permit ironic (baremetal) hosts to be reserved.'),
 ]
 
 plugin_opts.extend(monitor.monitor_opts)
@@ -609,6 +612,8 @@ class PhysicalHostPlugin(base.BasePlugin, nova.NovaClientWrapper):
                                                         **options)
         self.add_extra_allocation_info(hosts_allocations)
         self.add_allocation_cleaning_time(hosts_allocations, CONF.cleaning_time)
+        # Look up allocation usage for instance reservations
+        # Otherwise, we couldn't parse allocations for flavor/instance reservations
         inst_res_map = {}
         for allocation_list in hosts_allocations.values():
             for allocation in allocation_list:
@@ -780,7 +785,10 @@ class PhysicalHostPlugin(base.BasePlugin, nova.NovaClientWrapper):
         else:
             hosts = db_api.reservable_host_get_all_by_queries(filter_array)
         for host in hosts:
-            if not self.is_project_allowed(project_id, self.get_computehost(host["id"])):
+            full_host = self.get_computehost(host["id"])
+            if CONF[self.resource_type].filter_vm_hosts and full_host.get('hypervisor_type') != 'ironic':
+                continue
+            if not self.is_project_allowed(project_id, full_host):
                 continue
             if not db_api.host_allocation_get_all_by_values(
                     compute_host_id=host['id']):
