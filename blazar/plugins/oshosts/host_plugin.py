7ae72ea592b9532b83e53da383738b26ef285226
Michael Sherman <shermanm@uchicago.edu>
2026-07-28 14:29:15 +0000

    Fix crash cleaning up hosts without a reservation
    poll_resource_failures logs an error referencing the most recent
    physical:host reservation, but crashes if there has never been one.
    For now, skip the host in this case.
    
    TODO: We should clean up properly by making sure the host is in the
    freepool instead of skipping.
    
    First introduced in 6a6010f8 "Host aggregate cleanup - UTC time fix and better logging (#96)"
    Fix extracted from chameleoncloud/2023.1-kvm-rebase 7d80cf73.
    
    Co-authored-by: Mark Powers <markpowers@uchicago.edu>
    Change-Id: I3ace395855f665e10282e7b3eb2b3ab0e1d32af1

--- blazar/plugins/oshosts/host_plugin.py
+++ blazar/plugins/oshosts/host_plugin.py
@@ -1115,6 +1115,9 @@ class PhysicalHostMonitorPlugin(monitor.GeneralMonitorPlugin,
             for host in all_hosts:
                 # get the most recent reservation for the host_id and check if we need to move to freepool
                 reservation = db_utils.get_most_recent_reservation_info_by_host_id(host['id'])
+                # Ignore host if no host reservation exists for it
+                if not reservation:
+                    continue
                 # ignore the reservation which is active, as the host must already be in the right pool
                 if reservation and reservation["reservation_status"] == status.reservation.ACTIVE:
                     LOG.debug(f"{host['hypervisor_hostname']} is in an active reservation"
