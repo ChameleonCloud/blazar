c896ebabc69b366ac442e8261b5bebb46b44cfac
Michael Sherman <shermanm@uchicago.edu>
2026-09-22 16:15:26 +0000

    merge fixup: restore base.client_kwargs in nova client
    57e187e1 took upstream's BlazarNovaClient, reverting 716630c6. Upstream
    only does a password grant when passed a username, and our
    NovaClientWrapper doesn't pass one, so lease events run with a null token.
    
    Also drops the two TestCNClient tests that assert upstream's constructor.
    
    note: this also means that CONF.nova.endpoint_type is not read in our
    fork.
    
    TODO:  investigate how to handle all of the clients
    in a consistent way:
    
    - honor keystoneauth.loading's common config args
    - support service scoped auth
    - default to auth as the requesting user, allow override to auth as
      service user.
    
    These todos are probably upstreamable.

--- blazar/tests/utils/openstack/test_nova.py
+++ blazar/tests/utils/openstack/test_nova.py
@@ -53,54 +53,6 @@ class TestCNClient(tests.TestCase):
         self.version = '2'
         self.endpoint_type = 'internalURL'
 
-    def test_client_from_kwargs(self):
-        self.ctx.side_effect = RuntimeError
-        endpoint = 'fake_endpoint'
-        username = 'blazar_admin'
-        password = 'blazar_password'
-        user_domain = 'User_Domain'
-        project_name = 'admin'
-        project_domain = 'Project_Domain'
-        auth_url = "%s://%s:%s" % (CONF.os_auth_protocol,
-                                   CONF.os_auth_host,
-                                   CONF.os_auth_port)
-        if CONF.os_auth_prefix:
-            auth_url += "/%s" % CONF.os_auth_prefix
-
-        kwargs = {'version': self.version,
-                  'endpoint_override': endpoint,
-                  'username': username,
-                  'password': password,
-                  'user_domain_name': user_domain,
-                  'project_name': project_name,
-                  'project_domain_name': project_domain}
-
-        self.nova.BlazarNovaClient(**kwargs)
-
-        self.client.assert_called_once_with(version=self.version,
-                                            username=username,
-                                            password=password,
-                                            user_domain_name=user_domain,
-                                            project_name=project_name,
-                                            project_domain_name=project_domain,
-                                            auth_url=auth_url,
-                                            endpoint_override=endpoint,
-                                            endpoint_type=self.endpoint_type)
-
-    def test_client_from_ctx(self):
-        kwargs = {'version': self.version}
-
-        self.nova.BlazarNovaClient(**kwargs)
-
-        self.auth.assert_called_once_with(self.url,
-                                          self.ctx().auth_token)
-        self.session.assert_called_once_with(auth=self.auth.return_value)
-        self.client.assert_called_once_with(version=self.version,
-                                            endpoint_override=self.url,
-                                            endpoint_type=self.endpoint_type,
-                                            session=self.session.return_value,
-                                            global_request_id=mock.ANY)
-
     def test_getattr(self):
         # TODO(n.s.): Will be done as soon as pypi package will be updated
         pass
